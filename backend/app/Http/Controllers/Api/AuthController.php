0e757d9af63a83413d00a9b88470a17285653060
younes <younesabourrig1ya@gmail.com>
2026-03-13 12:18:19 +0000

    fix some errors

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -113,7 +113,8 @@ class AuthController extends Controller
 
         return response()->json([
             'user' => $user,
-            'token' => $token
+            'token' => $token,
+            'message' => 'logged in'
         ]);
     }
 
